9797f8afcccc747b455c357e4d58e8cda876c3e7
Noah <noah@noahveltman.com>
2016-08-10 17:20:19 -0400

    Removing dependencies

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var cov = require("compute-covariance"),
-    math = require("mathjs");
-
 function mahalanobis(data) {
 
   if (!Array.isArray(data)) {
@@ -17,34 +14,23 @@ function mahalanobis(data) {
     return [];
   }
 
-  var columns = transpose(data);
+  var columns = transpose(data),
+      means = columns.map(mean),
+      invertedCovariance = invert(cov(columns, means));
 
-  var covariance = cov(columns);
+  return data.map(function(row, i){
 
-  console.log(covariance);
-  var invertedCovariance = invert(covariance);
-  console.log(invertedCovariance);
-  console.log("");
-
-  var means = columns.map(function(column){
-    return mean(column);
-  });
-
-  var deltas = data.map(function(row){
-    return row.map(function(d,i){
+    var deltas = row.map(function(d, i){
       return d - means[i];
     });
-  });
-
-  return deltas.map(function(row,i){
 
     return Math.sqrt(
-      math.multiply(
-        math.multiply(
-          row,
+      multiply(
+        multiply(
+          deltas,
           invertedCovariance
         ),
-        row
+        deltas
       )
     );
 
@@ -52,15 +38,118 @@ function mahalanobis(data) {
 
 }
 
+function dot(a, b) {
+  if (a.length !== b.length) {
+    throw new TypeError("Vectors are of different sizes");
+  }
+
+  return sum(a.map(function(x, i){
+    return x * b[i];
+  }));
+}
+
+function multiply(a, b) {
+
+  var aSize = a.every(isNumeric) ? 1 : a.length,
+      bSize = b.every(isNumeric) ? 1 : b.length;
+
+  if (aSize === 1) {
+    if (bSize === 1) {
+      return dot(a, b);
+    }
+    return b.map(function(row){
+      return dot(a, row);
+    });
+  }
+
+  if (bSize === 1) {
+    return a.map(function(row){
+      return dot(row, b);
+    });
+  }
+
+  return a.map(function(x){
+    return transpose(b).map(function(y){
+      return dot(x, y);
+    });
+  });
+
+}
+
 function invert(matrix) {
 
-  return math.inv(matrix);
+  var size = matrix.length,
+      base,
+      swap,
+      augmented;
+
+  // Augment w/ identity matrix
+  augmented = matrix.map(function(row,i){
+    return row.slice(0).concat(row.slice(0).map(function(d,j){
+      return j === i ? 1 : 0;
+    }));
+  });
+
+  // Process each row
+  for (var r = 0; r < size; r++) {
+
+    base = augmented[r][r];
+
+    // Zero on diagonal, swap with a lower row
+    if (!base) {
+
+      for (var rr = r + 1; rr < size; rr++) {
+
+        if (augmented[rr][r]) {
+          // swap
+          swap = augmented[rr];
+          augmented[rr] = augmented[r];
+          augmented[r] = swap;
+          base = augmented[r][r];
+          break;
+        }
+
+      }
+
+      if (!base) {
+        throw new RangeError("Matrix not invertable.");
+      }
+
+    }
+
+    // 1 on the diagonal
+    for (var c = 0; c < size * 2; c++) {
+
+      augmented[r][c] = augmented[r][c] / base;
+
+    }
+
+    // Zeroes elsewhere
+    for (var q = 0; q < size; q++) {
+
+      if (q !== r) {
+
+        base = augmented[q][r];
+
+        for (var p = 0; p < size * 2; p++) {
+            augmented[q][p] -= base * augmented[r][p];
+        }
+
+      }
+
+    }
+
+  }
+
+  return augmented.map(function(row){
+    return row.slice(size);
+  });
 
 }
 
 function transpose(matrix) {
 
-  return matrix[0].map(function(d,i){
+  return matrix[0].map(function(d, i){
 
     return matrix.map(function(row){
       return row[i];
@@ -71,13 +160,32 @@ function transpose(matrix) {
 }
 
 function mean(arr) {
-  return arr.reduce(function(a,b){
+  return sum(arr) / arr.length;
+}
+
+function sum(arr) {
+  return arr.reduce(function(a, b){
     return a + b;
-  }) / arr.length;
+  });
 }
 
 function isNumeric(n) {
   return typeof n === "number" && !isNaN(n);
+}
+
+function cov(columns, means) {
+
+  return columns.map(function(c1, i){
+    return columns.map(function(c2, j){
+
+      var terms = c1.map(function(x, k){
+        return (x - means[i]) * (c2[k] - means[j]);
+      });
+
+      return sum(terms) / (c1.length - 1);
+    });
+  });
+
 }
 
 module.exports = mahalanobis;
